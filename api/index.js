fe2136bc269ed98e2fe7d33104f4443cdc76a99a
haitd <haitd@vbee.ai>
2024-03-22 10:01:51 +0700

    npx generator

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/apps", (req, res) => res.send("Express on Vercel"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
